c4e35c945aefb78b6016288ff1c44e81156a5273
Ashray Mehta <ashraymehta@users.noreply.github.com>
2020-06-30 19:19:34 +0530

    Added ritekit.gateway. Using @typed-rest-client to make network calls.

---
+++ src/gateways/ritekit.gateway.ts
@@ -0,0 +1,27 @@
+import {once} from 'events';
+import {injectable} from 'inversify';
+import {getLogger} from '../utils/logger.util';
+import {ConfigUtil} from '../utils/config.util';
+import {HttpClient} from 'typed-rest-client/HttpClient';
+
+@injectable()
+export class RitekitGateway {
+  private readonly configUtil: ConfigUtil;
+  private readonly logger = getLogger(RitekitGateway.name);
+
+  constructor(configUtil: ConfigUtil) {
+    this.configUtil = configUtil;
+  }
+
+  public async getCompanyLogo(domain: string) {
+    const httpClient = new HttpClient(undefined);
+    const ritekiteApiKey = await this.configUtil.getRitekitApiKey();
+    this.logger.info(
+      `Requesting logo for domain [${domain}] with api key [${ritekiteApiKey}].`,
+    );
+    const clientResponse = await httpClient.get(
+      `https://api.ritekit.com?client_id=${ritekiteApiKey}`,
+    );
+    return await once(clientResponse.message, 'data');
+  }
+}
--- src/utils/config.util.ts
+++ src/utils/config.util.ts
@@ -5,14 +5,6 @@ import {injectable} from 'inversify';
 @injectable()
 export class ConfigUtil {
   public async getRitekitApiKey(): Promise<string> {
-    const configFilePath = process.env['RITEKIT_CONFIG'] as string;
-    if (configFilePath) {
-      const ritekitConfig = JSON.parse(
-        fs.readFileSync(configFilePath, {encoding: 'utf-8'}),
-      );
-      return ritekitConfig.apiKey;
-    } else {
-      return '';
-    }
+    return process.env['RITEKIT_API_KEY'] as string;
   }
 }
