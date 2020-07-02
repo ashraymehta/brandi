63555e30644a492dc47147e4365c3a72a56c8ab6
ashray <ashraymehta@users.noreply.github.com>
2020-07-02 10:07:05 +0530

    Removed unused files. Minor code-cleanup.

--- src/gateways/ritekit.gateway.ts
+++ src/gateways/ritekit.gateway.ts
@@ -15,13 +15,13 @@ export class RitekitGateway {
   public async getCompanyLogo(
     domain: string,
   ): Promise<{logo: Buffer; contentType: string}> {
-    const ritekiteApiKey = await this.configUtil.getRitekitApiKey();
+    const ritekitApiKey = await this.configUtil.getRitekitApiKey();
     const ritekitApiBaseUrl = await this.configUtil.getRitekitApiBaseUrl();
     this.logger.info(
       `Requesting logo for domain [${domain}] from [${ritekitApiBaseUrl}].`,
     );
     const response = await superagent.get(
-      `${ritekitApiBaseUrl}v1/images/logo/?client_id=${ritekiteApiKey}&domain=${domain}`,
+      `${ritekitApiBaseUrl}v1/images/logo/?client_id=${ritekitApiKey}&domain=${domain}`,
     );
     return {
       logo: response.body as Buffer,
--- src/utils/dependency-injection.keys.ts
+++
@@ -1,4 +0,0 @@
-export enum DependencyInjectionKeys {
-  CUSTOM_SEARCH = 'brandi.custom-search',
-  CONFIG_UTIL = 'brandi.config-util',
-}
