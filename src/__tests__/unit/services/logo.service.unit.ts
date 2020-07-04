58bd5060716b31af77102935148f1a1c872ebc79
ashray <ashraymehta@users.noreply.github.com>
2020-07-04 13:29:05 +0530

    Stopped performing google search in logo.service.

--- src/__tests__/unit/services/logo.service.unit.ts
+++ src/__tests__/unit/services/logo.service.unit.ts
@@ -1,34 +1,19 @@
 import 'reflect-metadata';
-import {URL} from 'url';
 import {expect} from 'chai';
 import {instance, mock, when} from 'ts-mockito';
-import {BrandService, GoogleSearchService, LogoService} from '../../../services';
+import {BrandService, LogoService} from '../../../services';
 
 describe(LogoService.name, function () {
   const brandService = mock<BrandService>();
-  const googleSearchService = mock<GoogleSearchService>();
-  const logoService = new LogoService(instance(brandService), instance(googleSearchService));
+  const logoService = new LogoService(instance(brandService));
 
   it('should get logo for brand name', async function () {
     const logo = Buffer.of();
     const brandName = 'reddit';
-    const domain = 'reddit.com';
-    const url = new URL('https://reddit.com');
-    when(googleSearchService.findWebsite(brandName)).thenResolve(url);
-    when(brandService.findLogo(domain)).thenResolve({logo, contentType: 'image/png'});
+    when(brandService.findLogo(brandName)).thenResolve({logo, contentType: 'image/png'});
 
     const result = await logoService.getFor(brandName);
 
     expect(result).to.deep.equal({logo, contentType: 'image/png'});
-  });
-
-  it('should return null if URL is not found for brand name', async function () {
-    const brandName = 'something';
-    const url = undefined;
-    when(googleSearchService.findWebsite(brandName)).thenResolve(url);
-
-    const result = await logoService.getFor(brandName);
-
-    expect(result).to.be.null;
   });
 });
--- src/services/logo.service.ts
+++ src/services/logo.service.ts
@@ -1,23 +1,16 @@
 import {injectable} from 'inversify';
 import {BrandService} from './brand.service';
-import {GoogleSearchService} from './google-search.service';
 
+// TODO - Remove this proxy class
 @injectable()
 export class LogoService {
-  private readonly googleSearchService: GoogleSearchService;
   private readonly brandService: BrandService;
 
-  constructor(brandService: BrandService, googleSearchService: GoogleSearchService) {
-    this.googleSearchService = googleSearchService;
+  constructor(brandService: BrandService) {
     this.brandService = brandService;
   }
 
   async getFor(brandName: string): Promise<{logo: Buffer; contentType: string} | null> {
-    const url = await this.googleSearchService.findWebsite(brandName);
-    if (url) {
-      return await this.brandService.findLogo(url.host);
-    } else {
-      return null;
-    }
+    return await this.brandService.findLogo(brandName);
   }
 }
