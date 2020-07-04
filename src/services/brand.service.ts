ef398b0398908a36208052d084d5775523b89e52
ashray <ashraymehta@users.noreply.github.com>
2020-07-04 17:06:55 +0530

    Added logs in brand.service.

--- src/services/brand.service.ts
+++ src/services/brand.service.ts
@@ -1,5 +1,6 @@
 import {injectable} from 'inversify';
 import {Brand} from '../models/brand.model';
+import {getLogger} from '../utils/logger.util';
 import {Prefix, S3Gateway} from '../gateways/s3.gateway';
 import {RitekitGateway} from '../gateways/ritekit.gateway';
 import {GoogleSearchService} from './google-search.service';
@@ -11,6 +12,7 @@ export class BrandService {
   private readonly ritekitGateway: RitekitGateway;
   private readonly brandRepository: BrandRepository;
   private readonly googleSearchService: GoogleSearchService;
+  private readonly logger = getLogger(BrandService.name);
 
   constructor(
     googleSearchService: GoogleSearchService,
@@ -28,12 +30,16 @@ export class BrandService {
     name = name.toLowerCase();
     const existingBrand = await this.brandRepository.findByName(name);
     if (existingBrand) {
+      this.logger.info(`Found existing brand-info for brand-name [${name}].`);
       const {buffer, contentType} = await this.s3Gateway.get(existingBrand.logoKey);
       return {logo: buffer, contentType: contentType};
     }
 
+    this.logger.info(`Could not find existing brand-info for brand-name [${name}]. Trying to created one now...`);
+
     const brand = await this.createBrandFor(name);
     if (!brand) return undefined;
+
     const {buffer, contentType} = await this.s3Gateway.get(brand.logoKey);
     return {logo: buffer, contentType: contentType};
   }
@@ -51,6 +57,8 @@ export class BrandService {
 
     const brand = new Brand(name, domain, key);
     await this.brandRepository.insert(brand);
+
+    this.logger.info(`Saved brand information for brand-name [${name}].`);
     return brand;
   }
 }
