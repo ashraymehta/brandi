e2e40c7d6ce019ebe23805f68f6710a8bce14b8a
ashray <ashraymehta@users.noreply.github.com>
2020-07-08 21:51:28 +0530

    Added logs in image-processor.

--- src/services/image-processor.ts
+++ src/services/image-processor.ts
@@ -1,10 +1,12 @@
 import sharp = require('sharp');
 import {injectable} from 'inversify';
+import {getLogger} from '../utils/logger.util';
 import {RemoveBgGateway} from '../gateways/remove-bg.gateway';
 
 @injectable()
 export class ImageProcessor {
   private readonly removeBgGateway: RemoveBgGateway;
+  private readonly logger = getLogger(ImageProcessor.name);
 
   constructor(removeBgGateway: RemoveBgGateway) {
     this.removeBgGateway = removeBgGateway;
@@ -13,6 +15,8 @@ export class ImageProcessor {
   public async process(buffer: Buffer, contentType: string): Promise<{image: Buffer; contentType: string}> {
     const stats = await sharp(buffer).stats();
     if (!stats.isOpaque) return {image: buffer, contentType};
+
+    this.logger.info(`Image is detected to be opaque. Requesting background removal.`);
     return await this.removeBgGateway.removeBackground(buffer);
   }
 }
